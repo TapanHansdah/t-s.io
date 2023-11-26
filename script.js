47d6e0ec1f0c9786d8dab7d34bee34bd8ff0f83e
Tapan kumar hansdah <106474401+TapanHansdah@users.noreply.github.com>
2023-11-26 07:28:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,6 @@ document.addEventListener("DOMContentLoaded", function () {
         var scrollPosition = window.scrollY;
         var opacity = scrollPosition / 100; // Adjust the divisor to control the speed of the change
 
-        container.style.background = `linear-gradient(to right, rgba(255, 255, 255, ${1 - opacity}), rgba(0, 0, 0, ${opacity}))`;
+        container.style.background = `linear-gradient(to bottom, rgba(255, 255, 255, ${1 - opacity}), rgba(0, 0, 0, ${opacity}))`;
     });
 });
